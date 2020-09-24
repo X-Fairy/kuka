fef15c6c764590d2d0bb5933f87993038ed60c8f
unknown <1648089680@qq.com>
2020-09-24 16:30:56 +0800

    完善

--- src/components/Moperation.vue
+++ src/components/Moperation.vue
@@ -4,7 +4,7 @@
     <div class="content-in">
       <div class="search-out">
         <div class="search-in">
-          <!-- <i class="el-icon-search search-self"></i> -->
+          <i class="el-icon-search search-self"></i>
           <el-autocomplete
             class="inline-input"
             v-model="inputValue"
@@ -20,29 +20,32 @@
       <div class="active-instructions">
         <div class="active-title">
           <div class="cir"></div>
-          活动说明
+          {{ rulesObj.title }}
           <div class="cir"></div>
         </div>
         <div class="active-rules">
-          购买1元礼包且10.1日0-2点实付满2千元即可送乳胶枕一个 活动细则： ①
-          9.22-9.30日活动期间每个用户ID限拍一个1元礼包，且仅适用1次，多买无效；
-          ② 如拍下后，若未在活动期间成功支付商品，则视为自动放弃1元特权； ③
-          赠品将于购买商品确认收货后发出，如有问题可详询客服，实付金额不包括已使用的优惠券、购物津贴、拍下立减、折扣、红包、补贴、基金、五包费等费用；
-          ④ 若发生退款或退货行为，赠品将不予以发货； ⑤
-          礼包仅限天猫店（顾家家居官方旗舰店）使用。
+          <div class="rules-reward">{{ rulesObj.reward }}</div>
+          <div class="rules-remark">{{ rulesObj.remark }}</div>
+          <div
+            class="rules-list"
+            v-for="(it, index) in rulesObj.rulesData"
+            :key="index"
+          >
+            {{ it }}
+          </div>
         </div>
       </div>
-      <!-- 弹窗 -->
-      <div class="popup" :class="popupClass">
-        <div class="mask"></div>
-        <div class="layer">
-          <div class="info">
-            <div class="border1"></div>
-            <div class="border2"></div>
-            <img src="../assets/img/logo.png" alt="" class="logo" />
-            <p class="title">{{ tips }}</p>
-            <p class="subtitle">—— 您获得 ——</p>
-          </div>
+    </div>
+    <!-- 弹窗 -->
+    <div class="popup" :class="popupClass">
+      <div class="mask"></div>
+      <div class="layer">
+        <div class="info">
+          <div class="border1"></div>
+          <div class="border2"></div>
+          <img src="../assets/img/logo.png" alt="" class="logo" />
+          <p class="title">{{ tips }}</p>
+          <p class="subtitle">—— 您获得 ——</p>
         </div>
       </div>
     </div>
@@ -55,7 +58,19 @@ export default {
     return {
       inputValue: "",
       restaurants: [],
-      popupClass: "show",
+      rulesObj: {
+        title: "活动说明",
+        reward: "购买1元礼包且10.1日0-2点实付满2千元即可送乳胶枕一个",
+        remark: "活动细则：",
+        rulesData: [
+          "① 9.22-9.30日活动期间每个用户ID限拍一个1元礼包，且仅适用1次，多买无效；",
+          "② 如拍下后，若未在活动期间成功支付商品，则视为自动放弃1元特权；",
+          "③ 赠品将于购买商品确认收货后发出，如有问题可详询客服，实付金额不包括已使用的优惠券、购物津贴、拍下立减、折扣、红包、补贴、基金、五包费等费用；",
+          "④ 若发生退款或退货行为，赠品将不予以发货；",
+          "⑤ 礼包仅限天猫店（顾家家居官方旗舰店）使用。",
+        ],
+      },
+      popupClass: "",
       tips: "恭喜您，中奖了",
     };
   },
@@ -83,60 +98,64 @@ export default {
 };
 </script>
 <style   scoped>
-.mobile-content {
-  width: 100%;
-  height: 100%;
+.mobile-content{
+    width: 100%;
+    height:100vh;
+    background-color: #B41C2C;
 }
-
-.banner {
-  width: 100%;
-  height: 135px;
-  display: block;
-  margin-top: 10px;
+.header-title{
+    width: 100%;
+    text-align: center;
 }
-.content-in {
+.banner{
+    width: 100%;
+    height: 135px;
+    display: block;
+    /* margin-top: 10px; */
+}
+.content-in{
   width: 100%;
   height: auto;
-  background-color: #b41c2c;
+  background-color:  #B41C2C; 
   padding-bottom: 30px;
   position: relative;
 }
 
 /* 搜索开始 */
-.search-out {
-  width: 90%;
-  height: 40px;
-  line-height: 40px;
-  position: absolute;
-  top: -30px;
-  left: 5%;
-  background-color: #ffffff;
-  border-radius: 5px;
+.search-out{
+    width: 90%;
+    height: 40px;
+    line-height: 40px;
+    position: absolute;
+    top: -30px;
+    left: 5%;
+    background-color: #ffffff;
+    border-radius: 5px;
 }
-.search-in {
-  width: 100%;
-  height: 100%;
-  text-align: left;
-  border-radius: 5px;
-  background-color: #e42727;
-  position: relative;
-  overflow: hidden;
+.search-in{
+    width: 100%;
+    height: 100%;
+    text-align: left;
+    border-radius: 5px;
+    background-color: #E42727;
+    position: relative;
+    overflow: hidden;
 }
-.search-self {
-  color: #b2b2b2;
-  font-size: 14px;
-  text-align: left;
-  display: inline-block;
-  position: absolute;
-  left: 2%;
-  line-height: 40px;
-  z-index: 100;
+.search-self{
+    color: #B2B2B2;
+    font-size: 14px;
+    text-align: left;
+    display: inline-block;
+    position: absolute;
+    left: 2%;
+    line-height: 40px;
+    z-index: 100;
 }
-.inline-input {
-  width: 79%;
+.inline-input{
+    width: 79%; 
 }
 .search-in /deep/ .el-input {
-  width: 100%;
+    width: 100%;
 }
 .search-in /deep/ .el-input__inner {
   border: 1px solid #e42727;
@@ -146,46 +165,57 @@ export default {
   padding-left: 25px;
   background: #fff5f5;
 }
-.search-bt {
-  width: 19%;
-  text-align: center;
-  display: inline-block;
-  background-color: #e42727;
-  font-size: 14px;
-  color: #ffffff;
+.search-bt{
+    width: 19%;
+    text-align: center; 
+    display: inline-block;
+    background-color: #E42727;
+    font-size: 14px;
+    color: #ffffff;
 }
 /* 搜索结束 */
 
-.null-box {
-  width: 100%;
-  height: 30px;
+.null-box{
+    width: 100%;
+    height: 30px;
 }
 /* 活动数名 */
-.active-instructions {
-  width: 90%;
-  background: #ce3131;
-  border: 2px solid #ee6057;
-  box-sizing: border-box;
-  border-radius: 8px;
-  margin: 0 auto;
-  color: #ffe2b7;
+.active-instructions{
+    width: 90%;
+    background: #CE3131;
+    border: 2px solid #EE6057;
+    box-sizing: border-box;
+    border-radius: 8px;
+    margin: 0 auto ;
+    color: #FFE2B7;
 }
-.active-title {
-  font-size: 20px;
-  text-align: center;
-  margin-top: 22px;
-  margin-bottom: 16px;
+.active-title{
+    font-size: 20px;
+    text-align: center;
+    margin-top: 22px;
 }
-.active-rules {
-  font-size: 14px;
-  line-height: 22px;
+.active-rules{
+    font-size: 14px;
+    line-height: 22px;
+    padding: 20px;
 }
-.cir {
-  width: 6px;
-  height: 6px;
-  background: #ffedab;
-  border-radius: 50%;
-  display: inline-block;
+.rules-reward{
+    text-align: left;
+    font-size: 14px;
+}
+.rules-list{
+    text-align: left;
+    line-height: 22px;
+}
+.rules-remark{
+     text-align: left;
+}
+.cir{
+    width: 6px;
+    height: 6px;
+    background: #FFEDAB;
+    border-radius: 50%;
+    display: inline-block;
 }
 @keyframes showPopup {
   0% {
