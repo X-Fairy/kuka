926ef1dbcff1009058a6ce73a93242b47e691a32
huwt <huwt@huwtdeMacBook-Pro.local>
2020-09-24 16:11:58 +0800

    html

--- src/components/mobile.vue
+++ src/components/mobile.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="mobile-content">
-    <p class="header-title">顾家家居——中奖详情</p>
+    <!-- <p class="header-title">顾家家居——中奖详情</p> -->
     <img alt="宣传图" src="../assets/img/banner.png" class="banner" />
     <div class="content-in">
         <div class="search-out">
@@ -21,17 +21,13 @@
         <div class="active-instructions">
             <div class="active-title">
                 <div class="cir"></div>
-                活动说明
+                {{rulesObj.title}}
                 <div class="cir"></div>
             </div>
             <div class="active-rules">
-                购买1元礼包且10.1日0-2点实付满2千元即可送乳胶枕一个
-                活动细则：
-                ① 9.22-9.30日活动期间每个用户ID限拍一个1元礼包，且仅适用1次，多买无效；
-                ② 如拍下后，若未在活动期间成功支付商品，则视为自动放弃1元特权；
-                ③ 赠品将于购买商品确认收货后发出，如有问题可详询客服，实付金额不包括已使用的优惠券、购物津贴、拍下立减、折扣、红包、补贴、基金、五包费等费用；
-                ④ 若发生退款或退货行为，赠品将不予以发货；
-                ⑤ 礼包仅限天猫店（顾家家居官方旗舰店）使用。
+                <div class="rules-reward">{{rulesObj.reward}}</div>
+                <div class="rules-remark">{{rulesObj.remark}}</div>
+                <div class="rules-list" v-for="(it,index) in (rulesObj.rulesData)" :key="index">{{it}}</div>
             </div>
         </div>
     </div>
@@ -44,7 +40,19 @@ export default {
     return {
         inputValue:"",
         restaurants: [],
-        rulesData:[""]
+        rulesObj:{
+            title:"活动说明",
+            reward:"购买1元礼包且10.1日0-2点实付满2千元即可送乳胶枕一个",
+            remark:"活动细则：",
+            rulesData:[
+                "① 9.22-9.30日活动期间每个用户ID限拍一个1元礼包，且仅适用1次，多买无效；",
+                "② 如拍下后，若未在活动期间成功支付商品，则视为自动放弃1元特权；",
+                "③ 赠品将于购买商品确认收货后发出，如有问题可详询客服，实付金额不包括已使用的优惠券、购物津贴、拍下立减、折扣、红包、补贴、基金、五包费等费用；",
+                "④ 若发生退款或退货行为，赠品将不予以发货；",
+                "⑤ 礼包仅限天猫店（顾家家居官方旗舰店）使用。"
+            ]
+        },
+        
     };
   },
   methods: {
@@ -74,7 +82,8 @@ export default {
 
 .mobile-content{
     width: 100%;
-    height: 100%;
+    height:100vh;
+    background-color: #B41C2C;
 }
 .header-title{
     width: 100%;
@@ -84,7 +93,7 @@ export default {
     width: 100%;
     height: 135px;
     display: block;
-    margin-top: 10px;
+    /* margin-top: 10px; */
 }
 .content-in{
   width: 100%;
@@ -166,11 +175,22 @@ export default {
     font-size: 20px;
     text-align: center;
     margin-top: 22px;
-    margin-bottom: 16px;
 }
 .active-rules{
     font-size: 14px;
     line-height: 22px;
+    padding: 20px;
+}
+.rules-reward{
+    text-align: left;
+    font-size: 14px;
+}
+.rules-list{
+    text-align: left;
+    line-height: 22px;
+}
+.rules-remark{
+     text-align: left;
 }
 .cir{
     width: 6px;
